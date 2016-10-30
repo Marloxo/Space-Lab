f3d3c0953d5756a875602aa01a5fc0928e470549
Marloxo <marloo-_-@hotmail.com>
2016-10-30 23:03:18 +0200

    :sparkles: Implement Spawning objects on mouse click

---
+++ Space Lab Game/Assets/Script/MouseManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour
+{
+    // public Camera theCamera = Camera.main;
+    public Camera theCamera;
+    public GameObject prefabToSpawn;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //was the mouse pressed down this frame? 
+        if (Input.GetMouseButtonDown(0))
+        {            //Yes, the left mouse button
+                     //              //Is the mouse over a cube
+
+            //Ray ray = new Ray(theCamera.transform.position, theCamera.transform.forward);
+            Ray ray = theCamera.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                Debug.Log("We hit: " + hitInfo.collider.gameObject.name);
+                // Now let's spawn new object
+                Vector3 spawnSpot = hitInfo.point;
+                Instantiate(prefabToSpawn, spawnSpot, Quaternion.identity);
+            }
+        }
+    }
+}
